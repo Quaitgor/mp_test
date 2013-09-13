de433411b3eb99ec681721b72dc9ffd0ffd67494
C.Neff <quaitgor@hotmail.com>
2013-09-13 08:56:05 +0200

    v0.010

---
+++ mp_test/src/Models/GrahpicElement.java
@@ -0,0 +1,18 @@
+package Models;
+
+public class GrahpicElement {
+
+	public int x = 0;
+	public int y = 0;
+	public int rotation = 0;
+
+	public void init(){}
+	
+	public double[] getPosition(){
+		return new double[] {x,y};
+	}
+	public void setPosition(int nx, int ny){
+		this.x = nx;
+		this.y = ny;
+	}
+}
